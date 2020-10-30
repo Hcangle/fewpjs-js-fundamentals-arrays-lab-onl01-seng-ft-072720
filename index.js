1a123c22a7c99a6b1246d812870687708f1467f3
Hannah Angle <hannahcangle@gmail.com>
2020-10-30 17:21:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const append = ["Milo", "Otis", "Garfield"];
 append.push("Odie");
 const prepend = ["Milo", "Otis", "Garfield"];
-prepend.unshift("Meeko")
+prepend.unshift("Odie")
 const removeLast = ["Milo", "Otis", "Garfield"];
 removeLast.pop();
 const removeFirst = ["Milo", "Otis", "Garfield"];
